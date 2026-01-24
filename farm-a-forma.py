9f9248b1b6ea73d6ebcf12c2896e02e14e262863
MDRDPR <redgamerpr@gmail.com>
2026-01-24 15:45:35 -0400

    farm-a-forma-v1.0

--- farm-a-forma.py
+++ farm-a-forma.py
@@ -1,13 +1,24 @@
 import random
 
+# Choose a number between 0 and 6
+# If you choose incorrectly you get to farm a forma ;]
+while True:
+    try:
+        f = int(input("Guess a number between 20 and 0:"))
+        if f <= 0:
+            print("Try a valid number")
+            continue
+        break
+    except ValueError:
+        print("Please enter a valid number!")
 
-f = int(input("Guess a number between 20 and 0:"))
-
-x = random.randint(0, 20)
+x = random.randint(0, 6)
 
 if x == f:
+    print("=" * 12)
     print(f"Correct it was {x}")
     print("Congrats!!!")
 else:
+    print("=" * 12)
     print("You lose: Farm a Forma!!")
-    print(f"Wrong!!! It was {x}")
+    print(f"It was {x}")
